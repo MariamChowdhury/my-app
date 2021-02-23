e573d3e989cee03f63f36b0a531bc7d3f7256bb0
Mariam <mariamchowdhury.200@gmail.com>
2021-02-23 21:48:34 +0600

    stupidThings

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var properties={
+    name:'First',
+    language:'React'
+  }
+  var style={
+      color:'pink',
+      backgroundColor:'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 style={style}>{properties.name+" "+ properties.language} App!!</h1>
         <p>
           Edit done! <code>src/App.js</code> and save to reload.
         </p>
-        {/* <a
+
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
       </header>
     </div>
   );
